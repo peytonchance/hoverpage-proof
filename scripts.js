b38c29c7db44f9c3416f29108d47d0444fb70ffc
peytonchance <peytonchance@gmail.com>
2018-02-21 11:48:10 -0500

    Some slight style updates.

--- scripts.js
+++ scripts.js
@@ -22,22 +22,3 @@ function resetVideo(){
   video.load();
   video.play();
 }
-
-
-// Trying scripts for full-screen Vimeo
-function videoSize() {
-  var $windowHeight = $(window).height();
-  var $videoHeight = $(".video").outerHeight();
-	var $scale = $windowHeight / $videoHeight;
-
-  if ($videoHeight <= $windowHeight) {
-    $(".video").css({
-      "-webkit-transform" : "scale("+$scale+") translateY(-50%)",
-			"transform" : "scale("+$scale+") translateY(-50%)"
-		});
-	};
-}
-
-$(window).on('load resize',function(){
-  videoSize();
-});
